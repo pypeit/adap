a1d9758fde4f3f5e5ed19cdec0296de172cafea3
Dusty Reichwein <jreichwe@ucsc.edu>
2022-03-07 12:14:21 -0800

    Fix date range grouping when using batches.

--- scripts/adap_reorg_setup.py
+++ scripts/adap_reorg_setup.py
@@ -39,11 +39,9 @@ import datetime
 import traceback
 from operator import is_
 
-from astropy.table import Table, vstack, Column
-from astropy.time import Time
-from astropy.io import fits 
+from astropy.table import Table, vstack, Column, MaskedColumn
+from astropy.time import Time, TimeDelta
 import numpy as np
-from pypeit import spectrographs
 
 from pypeit.scripts import scriptbase
 from pypeit.spectrographs.util import load_spectrograph
@@ -154,15 +152,182 @@ def merge_tables(table1, table2):
         if (c1.dtype != table2[c1.name].dtype):
             if c1.dtype == np.dtype('object'):
                 # Convert Table 2's column to object
+                if isinstance(table2[c1.name], MaskedColumn):
+                    table2[c1.name].mask = np.zeros_like(table2[c1.name].mask, dtype=bool)
+
                 newcol = Column(table2[c1.name], c1.name, np.dtype('object'))
                 table2[c1.name] = newcol
             elif table2[c1.name].dtype == np.dtype('object'):
                 # Convert Table 1's column to object
+                if isinstance(table1[c1.name], MaskedColumn):
+                    table1[c1.name].mask = np.zeros_like(table1[c1.name].mask, dtype=bool)
+
                 newcol = Column(table1[c1.name], c1.name, np.dtype('object'))
                 table1[c1.name] = newcol
             # Else we hope vstack can handle it                
 
     return vstack([table1, table2])
+
+class DateGroup:
+    def __init__(self, start_date, end_date, parent_dir):
+        self.start_date = start_date
+        self.end_date = end_date
+        self.parent_dir = parent_dir
+
+        if start_date != end_date:
+            self.path = os.path.join(parent_dir, start_date.to_value('iso', subfmt='date') + "_" + end_date.to_value('iso', subfmt='date'))
+        else:
+            self.path = os.path.join(parent_dir, start_date.to_value('iso', subfmt='date'))
+
+    def merge(self, other_dg):
+
+        if self.parent_dir != other_dg.parent_dir:
+            raise ValueError("Can't merge date groups from different parent directories.")
+
+        start_date = self.start_date
+        end_date = self.end_date
+
+        if other_dg.start_date < self.start_date:
+            start_date = other_dg.start_date
+        if other_dg.end_date > self.end_date:
+            end_date = other_dg.end_date
+
+        return DateGroup(start_date, end_date, self.parent_dir)
+
+def get_dates(cfg_group):
+    dates = []
+    for mjd in cfg_group['mjd']:
+        try:
+            date = Time(mjd, format='mjd')#.to_value('iso', subfmt='date')
+        except:
+            # We have to keep an entry for this so that the column
+            # remains the same length as the table
+            date = Time('1970-01-01', format='mjd')
+        dates.append(date)
+
+
+_all_date_groups = dict()
+
+class DateGroup:
+    def __init__(self, id, start_date, end_date):
+        self.id = id
+        self.start_date = start_date
+        self.end_date = end_date
+
+    def add_date(self, date):
+        if date < self.start_date:
+            self.start_date = date
+        elif date > self.end_date:
+            self.end_date = date
+
+    def merge(self, other_dg):
+        if other_dg.start_date < self.start_date:
+            self.start_date = other_dg.start_date
+
+        if other_dg.end_date > self.end_date:
+            self.end_date = other_dg.end_date
+
+
+def get_date_groups(args, cfg_group_dir, cfg_group):
+    """Return an array that can groups the entries in a configuration group by a date range."""
+
+    window_delta = TimeDelta(args.date_window, format='jd')
+    date_group_ids = np.zeros(len(cfg_group),dtype=int)
+
+    # Because files are processed in batches, make sure to compare against date groups
+    # from all previous batches.  combined_date_groups has the group integer values form
+    # previous batches, and combined_dates has the date that correspond to that group.
+    if cfg_group_dir not in _all_date_groups:
+        #_all_date_groups[cfg_group_dir] = (np.empty(0, dtype=int), np.empty(0, dtype=int))
+        _all_date_groups[cfg_group_dir] = []
+
+    #(combined_date_groups, combined_dates) = _all_date_groups[cfg_group_dir]
+    date_groups = _all_date_groups[cfg_group_dir]
+
+    # next_group is the next integer to use for a date group if a file doesn't match any previous
+    # group. This starts at 1 because 0 is reserved for images without an assigned group
+    if len(date_groups) > 0:
+        next_group = np.max([x.id for x in date_groups]) + 1
+    else:
+        next_group = 1
+
+    # Get the dates for the images in this batch
+    dates = []
+    for mjd in np.trunc(cfg_group['mjd']):
+        try:
+            date = Time(mjd, format='mjd')#.to_value('iso', subfmt='date')
+        except:
+            # We have to keep an entry for this so that the column
+            # remains the same length as the table
+            date = Time('1970-01-01', format='mjd')
+        dates.append(date)
+
+
+    for i in range(len(dates)):
+        if len(date_groups) == 0:
+            date_groups.append(DateGroup(next_group, dates[i], dates[i]))
+            date_group_ids[i] = next_group
+            next_group += 1
+        else:
+            group = None
+            for j in range(len(date_groups)):
+                if date_groups[j] is None:
+                    # Skip groups that were set to None by merging
+                    continue
+
+                # Find the date window for this group and see if the current date
+                # fits into it
+                start = date_groups[j].start_date - window_delta
+                end = date_groups[j].end_date + window_delta
+                if dates[i] >= start and dates[i] <= end:
+                    if group is None:
+                        date_groups[j].add_date(dates[i])
+                        date_group_ids[i] = date_groups[j].id
+                        group = date_groups[j]                  
+                    else:
+                        # The new date belongs to two groups, so those groups should be merged
+                        
+                        # Merge new group into prior group
+                        group.merge(date_groups[j])
+                        
+                        # Set group id for current date
+                        date_group_ids[i] = group.id
+                        
+                        # Update group ids for dates associated with the old group
+                        old_idx = date_group_ids == date_groups[j].id
+                        date_group_ids[old_idx] = group.id
+                        
+                        # Remove the old group, but because we're iterating through the list
+                        # just set it to None
+                        date_groups[j] = None                      
+
+            if group is None:
+                # No match was found, start a new group
+                date_groups.append(DateGroup(next_group, dates[i], dates[i]))
+                date_group_ids[i] = next_group
+                next_group += 1
+
+    # Filter out Nones from merging before returning
+    _all_date_groups[cfg_group_dir] = [x for x in date_groups if x is not None]
+
+    return date_group_ids
+
+def rename_date_range_dirs(base_dir):
+    """Rename date range directories from the date range id to the 
+       actual start/end dates"""
+    for key in _all_date_groups.keys():
+        date_groups = _all_date_groups[key]
+        for dg in date_groups:
+            start_date_str = dg.start_date.to_value('iso', subfmt='date')
+            end_date_str   = dg.end_date.to_value('iso', subfmt='date')
+            if start_date_str == end_date_str:
+                new_name = dg.start_date.to_value('iso', subfmt='date')
+            else:
+                new_name = dg.start_date.to_value('iso', subfmt='date') + "_" + dg.end_date.to_value('iso', subfmt='date')
+
+            orig_dir = os.path.join(base_dir, key, str(dg.id))
+            new_dir = os.path.join(base_dir, key, new_name)
+            os.rename(orig_dir, new_dir)
 
 def transfer_batch(args, spectrograph, par, files):
     """Transfer a batch of files to their correct, grouped destinations.
@@ -185,44 +350,33 @@ def transfer_batch(args, spectrograph, par, files):
         print("Grouping by 'dispname', 'dispangle', 'filter1'")
         for cfg_group in decker_group.group_by(['dispname', 'dispangle', 'filter1']).groups:
 
-            dates = get_date_groups(args, cfg_group)
-            config_group_dir = f"{cfg_group['dispname'][0]}_{cfg_group['dispangle'][0]:.0f}_{cfg_group['filter1'][0]}".replace(" ", "_")
+            config_group_dir = f"{cfg_group['decker'][0]}{os.path.sep}{cfg_group['dispname'][0]}_{cfg_group['dispangle'][0]:.0f}_{cfg_group['filter1'][0]}".replace(" ", "_")
             print("Grouping by dates")
-            date_groups = cfg_group.group_by(np.array(dates)).groups
+            date_group_ids = get_date_groups(args, config_group_dir, cfg_group)
+            date_groups = cfg_group.group_by(date_group_ids).groups
             for i in range(len(date_groups)):
 
-                # Determine the name of the date group based on the min/max dates
-                min_date = Time(np.min(date_groups[i]['mjd']), format='mjd').to_value('iso', subfmt='date')
-                max_date = Time(np.max(date_groups[i]['mjd']), format='mjd').to_value('iso', subfmt='date')
-                if min_date != max_date:
-                    dg_name = f"{min_date}_{max_date}"
-                else:
-                    dg_name = min_date
-    
-                grouping_path = os.path.join(decker_group['decker'][0].replace(" ", "_"),
-                                             config_group_dir,
-                                             dg_name)
-                
-                
+                date_group_dir = os.path.join(config_group_dir, str(date_groups.keys[i]))
                 #complete_target = os.path.join(dest, "complete")
                 if cloudstorage.is_cloud_uri(args.source_dirs[0]):
-                    dest = os.path.join(args.local_out, grouping_path)
+                    dest = os.path.join(args.local_out,date_group_dir)
                 else:
-                    dest = os.path.join(args.out_dir, grouping_path)
+                    dest = os.path.join(args.out_dir,  date_group_dir)
                
                 incomplete_target = os.path.join(dest, "incomplete")
                 raw_dir = os.path.join(incomplete_target, "raw")
+
                 file_list = transfer_data(args, list(date_groups[i]['localfile']), raw_dir)
                 transferred_files += file_list
 
-                print(f"Creating PypeItMetaData for {grouping_path}")
+                print(f"Creating PypeItMetaData for {date_group_dir}")
                 metadata = PypeItMetaData(spectrograph, par, files=file_list)
                 # Keep original source information so that the files can be copied within S3 later
                 if cloudstorage.is_cloud_uri(args.source_dirs[0]):
                     source_dirs = [os.path.dirname(x['sourcefile']) for x in date_groups[i]]
                     metadata.table['directory'] = Column(data=source_dirs, name='directory')
 
-                metadata_file = os.path.join(incomplete_target, f"metadata_{grouping_path.replace(os.path.sep, '_')}.ecsv")
+                metadata_file = os.path.join(incomplete_target, f"metadata_{config_group_dir.replace(os.path.sep, '_')}.ecsv")
                 if os.path.exists(metadata_file):
                     # Merge with the results of a prior run
                     print(f"Merging with {metadata_file}")
@@ -230,7 +384,7 @@ def transfer_batch(args, spectrograph, par, files):
                     metadata.table = merge_tables(existing_metadata, metadata.table)
                 metadata.table.sort(['mjd', 'filename'])
                 metadata.table.write(metadata_file, format="ascii.ecsv",overwrite=True)
-                
+
     return transferred_files
 
 def is_bias_frame(spectrograph, headarr):
@@ -275,46 +429,6 @@ def read_grouping_metadata(args, spectrograph, files, local_files):
     else:
         return Table(rows=data_rows, names=keys, dtype=dtypes)
 
-def get_date_groups(args, cfg_group):
-    """Return an array that can groups the entries in a configuration group by a date range."""
-    date_groups = np.zeros(len(cfg_group))
-    next_frame = 0
-    next_group = 1
-
-    dates = []
-    for mjd in cfg_group['mjd']:
-        try:
-            date = Time(mjd, format='mjd')#.to_value('iso', subfmt='date')
-        except:
-            # We have to keep an entry for this so that the column
-            # remains the same length as the table
-            date = Time('1970-01-01', format='mjd')
-        dates.append(date)
-
-
-    for i in range(len(date_groups)):
-        if i == next_frame:
-            continue # Don't compare with self
-        if np.fabs((dates[i] - dates[next_frame]).value) <= args.date_window:
-            if date_groups[next_frame] == 0:
-                if date_groups[i] != 0:
-                    date_groups[next_frame] = date_groups[i]
-                else:
-                    date_groups[next_frame] = next_group
-                    date_groups[i] = next_group
-                    next_group += 1
-            else:
-                if date_groups[i] == 0:
-                    date_groups[i] = date_groups[next_frame]
-                elif date_groups[i] != date_groups[next_frame]:
-                    prev_entries_in_group = date_groups == date_groups[next_frame]
-                    date_groups[prev_entries_in_group] = date_groups[i]
-    for i in range(len(date_groups)):
-        if date_groups[i] == 0:
-            date_groups[i] = next_group
-            next_group += 1
-    return date_groups
-    # IS this really it?
 
 def get_files(args):
     """Find all of the source fits files, whether in S3 or local."""
@@ -400,6 +514,8 @@ class ReorgSetup(scriptbase.ScriptBase):
         else:
             metadata_search_path = args.out_dir
 
+        rename_date_range_dirs(metadata_search_path)
+
         incomplete_science_dirs = []
         files_with_unknown_type = []
         files_with_missing_metadata = []
@@ -409,7 +525,7 @@ class ReorgSetup(scriptbase.ScriptBase):
 
             rows_with_none = find_none_rows(metadata.table)
             for row in metadata.table[rows_with_none]:
-                files_with_missing_metadata.append(os.path.join(row['directory'], row['filename']))
+                files_with_missing_metadata.append(os.path.join(target_dir, "raw", row['filename']))
 
             metadata.table.remove_rows(rows_with_none)
 
@@ -419,7 +535,7 @@ class ReorgSetup(scriptbase.ScriptBase):
             # Find and filter out images with unknown types
             unknown_idx = np.logical_or(metadata.table["frametype"] == 'None', np.asarray(np.vectorize(is_)(metadata.table["frametype"], None)))
             for row in metadata.table[unknown_idx]:
-                files_with_unknown_type.append(os.path.join(row['directory'], row['filename']))
+                files_with_unknown_type.append(os.path.join(target_dir, "raw", row['filename']))
 
             metadata.table.remove_rows(unknown_idx)
 
@@ -429,17 +545,19 @@ class ReorgSetup(scriptbase.ScriptBase):
                     os.rename(target_dir, new_target_dir)
                     target_dir = new_target_dir
                     raw_dir = os.path.join(target_dir, "raw")
-                    if not cloudstorage.is_cloud_uri(args.out_dir):
-                        metadata.table['directory'] = Column(data=np.full(len(metadata.table), raw_dir), name='directory')
             else:
+                raw_dir = os.path.join(target_dir, "raw")
                 if len(metadata.table[metadata.find_frames("science")]) > 0:
                     incomplete_science_dirs.append(os.path.dirname(target_dir))
 
+            if not cloudstorage.is_cloud_uri(args.out_dir):
+                # Update directory with renamed value
+                metadata.table['directory'] = Column(data=np.full(len(metadata.table), raw_dir), name='directory')
+            else:
 
-            # Transfer files within the cloud, updating the metadata directory to point to the new cloud URI
-            # This has to be done after determining if the directory is complete, because there's no
-            # rename or move in S3
-            if cloudstorage.is_cloud_uri(args.out_dir):
+                # Transfer files within the cloud, updating the metadata directory to point to the new cloud URI
+                # This has to be done after determining if the directory is complete, because there's no
+                # rename or move in S3
                 cloud_dir = os.path.join(args.out_dir, os.path.relpath(target_dir, args.local_out), 'raw')
                 file_list = [os.path.join(row['directory'], row['filename']) for row in metadata.table]
                 transfer_data(args, file_list, cloud_dir)
