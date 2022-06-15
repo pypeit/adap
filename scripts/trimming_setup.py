fca2030de7c41b258594a0772ae77eb41f8e41de
profxj <xavier@ucolick.org>
2022-06-15 14:07:01 -0700

    time fix

--- scripts/trimming_setup.py
+++ scripts/trimming_setup.py
@@ -24,6 +24,8 @@ os.makedirs(target_dir, exist_ok=True)
 # Run setup
 ps.run(sort_dir=target_dir, setup_only=True)
 
+mjd_order = np.argsort(ps.fitstbl['mjd'])
+
 # Keep
 keep = np.ones(len(ps.fitstbl), dtype=bool)
 
@@ -31,12 +33,17 @@ keep = np.ones(len(ps.fitstbl), dtype=bool)
 best_dec = np.abs(ps.fitstbl['dec']-45.) < 1.
 best_arc_exp = (ps.fitstbl['exptime'] > 1.) & (ps.fitstbl['exptime'] <= 5.) 
 best_flat_exp = (ps.fitstbl['exptime'] > 5.) & (ps.fitstbl['exptime'] <= 20.) 
+test_criterion = (ps.fitstbl['exptime'] > 50000.)
+
+arc_type = 'arc,tilt'
+flat_type = 'pixelflat,illumflat,trace'
+min_arcs = 2
+min_flats = 3
 
 # Cut down Arcs
-arcs = ps.fitstbl['frametype'] == 'arc,tilt'
-min_arcs = 2
+arcs = ps.fitstbl['frametype'] == arc_type
 
-arc_criteria = [best_arc_exp, best_dec]
+arc_criteria = [best_arc_exp, best_dec, test_criterion]
 
 while True:
     criteria = np.stack([arcs]+arc_criteria)
@@ -45,16 +52,18 @@ while True:
     if np.sum(gd_arcs) > min_arcs or len(arc_criteria) == 0:
         break
     # Remove a criterion
+    print("Removing an arc criterion")
     arc_criteria.pop()
 
-# Keep the last ones
-keep_arcs = np.where(gd_arcs)[0][-min_arcs:]
+# Keep only the last ones in time
 keep[arcs] = False
-keep[keep_arcs] = True
+
+keep_arcs = np.where(gd_arcs)[0]
+sort_mjd_arcs = np.argsort(ps.fitstbl['mjd'].data[keep_arcs])
+keep[keep_arcs[sort_mjd_arcs[-min_arcs:]]] = True
 
 # Flats
-flats = ps.fitstbl['frametype'] == 'pixelflat,illumflat,trace'
-min_flats = 3
+flats = ps.fitstbl['frametype'] == flat_type
 
 flat_criteria = [best_flat_exp, best_dec]
 
@@ -65,12 +74,15 @@ while True:
     if np.sum(gd_flats) > min_flats or len(flat_criteria) == 0:
         break
     # Remove a criterion
+    print("Removing an flat criterion")
     flat_criteria.pop()
 
 # Keep the last ones
-keep_flats = np.where(gd_flats)[0][-min_flats:]
 keep[flats] = False
-keep[keep_flats] = True
+
+keep_flats = np.where(gd_flats)[0]
+sort_mjd_flats = np.argsort(ps.fitstbl['mjd'].data[keep_flats])
+keep[keep_flats[sort_mjd_flats[-min_flats:]]] = True
 
 # Finish
 ps.fitstbl.table = ps.fitstbl.table[keep]
