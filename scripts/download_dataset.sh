f007f98b0c3e2f6713effac40ca04bdb12108e97
Dusty Reichwein <dusty@ucolick.org>
2022-07-11 15:21:55 -0700

    Bug fixes in upload/download scripts

--- scripts/download_dataset.sh
+++ scripts/download_dataset.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-rel_path = "$1/complete/"
-s3_path = "s3://pypeit/adap/raw_data_reorg/$rel_path/"
+rel_path="$1/complete/"
+s3_path="s3://pypeit/adap/raw_data_reorg/$rel_path/"
 
 mkdir -p $rel_path
 
--- scripts/upload_results.sh
+++ scripts/upload_results.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rel_path = "$1/complete/reduce/"
-s3_path = "s3://pypeit/adap/raw_data_reorg/$rel_path/"
+rel_path="$1/complete/reduce/"
+s3_path="s3://pypeit/adap/raw_data_reorg/$rel_path/"
 
 aws --endpoint http://rook-ceph-rgw-nautiluss3.rook s3 cp --no-progress --recursive $rel_path $s3_path
